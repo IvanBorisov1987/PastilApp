06391cdbede8d41a95f2b8f1362e278c9d1d8f50
PillarDevelopment <2622610@gmail.com>
2018-04-12 09:53:46 +0300

    create

---
+++ Contracts/token/nonERC20.sol
@@ -0,0 +1,225 @@
+pragma solidity ^0.4.21;
+
+library SafeMath
+{
+    function mul(uint256 a, uint256 b) internal pure
+    returns (uint256)
+    {
+        uint256 c = a * b;
+
+        assert(a == 0 || c / a == b);
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure
+    returns (uint256)
+    {
+        uint256 c = a / b;
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure
+    returns (uint256)
+    {
+        assert(b <= a);
+
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure
+    returns (uint256)
+    {
+        uint256 c = a + b;
+
+        assert(c >= a);
+
+        return c;
+    }
+}
+
+contract Ownable
+{
+    address public owner;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+contract Migrations is Ownable {
+    uint256 public lastCompletedMigration;
+
+    function setCompleted(uint256 completed) onlyOwner  public {
+        lastCompletedMigration = completed;
+    }
+
+    function upgrade(address newAddress) onlyOwner  public {
+        Migrations upgraded = Migrations(newAddress);
+        upgraded.setCompleted(lastCompletedMigration);
+    }
+}
+
+interface tokenRecipient
+{
+    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public;
+}
+
+contract TokenERC20 is Ownable
+{
+    using SafeMath for uint;
+
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 DEC = 10 ** uint256(decimals);
+    address public owner;
+
+    uint256 public totalSupply;
+    uint256 public avaliableSupply;
+    uint256 public buyPrice = 12000 szabo;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    function TokenERC20(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public
+    {
+        totalSupply = initialSupply * DEC;  // Update total supply with the decimal amount
+        balanceOf[this] = totalSupply;                // Give the creator all initial tokens
+        avaliableSupply = balanceOf[this];            // Show how much tokens on contract
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        owner = msg.sender;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal
+    {
+        require(_to != 0x0);
+        require(balanceOf[_from] >= _value);
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        Transfer(_from, _to, _value);
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+    function transfer(address _to, uint256 _value) public
+    {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public
+    returns (bool success)
+    {
+        require(_value <= allowance[_from][msg.sender]);
+
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public
+    returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public onlyOwner
+    returns (bool success)
+    {
+        tokenRecipient spender = tokenRecipient(_spender);
+
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+    function increaseApproval (address _spender, uint _addedValue) public
+    returns (bool success)
+    {
+        allowance[msg.sender][_spender] = allowance[msg.sender][_spender].add(_addedValue);
+
+        Approval(msg.sender, _spender, allowance[msg.sender][_spender]);
+
+        return true;
+    }
+
+    function decreaseApproval (address _spender, uint _subtractedValue) public
+    returns (bool success)
+    {
+        uint oldValue = allowance[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowance[msg.sender][_spender] = 0;
+        } else {
+            allowance[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        Approval(msg.sender, _spender, allowance[msg.sender][_spender]);
+        return true;
+    }
+
+    function burn(uint256 _value) public onlyOwner
+    returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        avaliableSupply -= _value;
+        Burn(msg.sender, _value);
+        return true;
+    }
+
+    function burnFrom(address _from, uint256 _value) public onlyOwner
+    returns (bool success)
+    {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        avaliableSupply -= _value;
+        Burn(_from, _value);
+        return true;
+    }
+}
+
+contract ERC20Extending is TokenERC20
+{
+
+    function transferEthFromContract(address _to, uint256 amount) public onlyOwner
+    {
+        amount = amount * DEC;
+        _to.transfer(amount);
+    }
+
+    function transferTokensFromContract(address _to, uint256 _value) public onlyOwner
+    {
+        avaliableSupply -= _value;
+        _value = _value*DEC;
+        _transfer(this, _to, _value);
+    }
+}
