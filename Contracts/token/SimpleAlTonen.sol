a02bb83a65a1918295ee600792edb44f0349434e
PillarDevelopment <2622610@gmail.com>
2018-04-26 13:23:01 +0300

    Аналогичный контракт с StandartMintableBurnableToken который я сделал во время зависания Etherscan

---
+++ Contracts/token/SimpleAlTonen.sol
@@ -0,0 +1,149 @@
+pragma solidity ^0.4.21;
+
+import "https://github.com/OpenZeppelin/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+contract Ownable {
+
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+contract Pausable is Ownable {
+    event Pause();
+    event Unpause();
+
+    bool public paused = false;
+
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+
+    modifier whenPaused() {
+        require(paused);
+        _;
+    }
+
+    function pause() onlyOwner whenNotPaused public {
+        paused = true;
+        emit Pause();
+    }
+
+    function unpause() onlyOwner whenPaused public {
+        paused = false;
+        emit Unpause();
+    }
+}
+
+contract BurnableToken is BasicToken {
+
+    event Burn(address indexed burner, uint256 value);
+
+    function burn(uint256 _value) public {
+        _burn(msg.sender, _value);
+    }
+
+    function _burn(address _who, uint256 _value) internal {
+        require(_value <= balances[_who]);
+
+        balances[_who] = balances[_who].sub(_value);
+        totalSupply_ = totalSupply_.sub(_value);
+        emit Burn(_who, _value);
+        emit Transfer(_who, address(0), _value);
+    }
+}
+
+contract MintableToken is StandardToken, BurnableToken, Ownable {
+
+    event Mint(address indexed to, uint256 amount);
+    event MintFinished();
+
+    bool public mintingFinished = false;
+
+    modifier canMint() {
+        require(!mintingFinished);
+        _;
+    }
+
+    function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+        totalSupply_ = totalSupply_.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Mint(_to, _amount);
+        emit Transfer(address(0), _to, _amount);
+        return true;
+    }
+
+    function finishMinting() onlyOwner canMint public returns (bool) {
+        mintingFinished = true;
+        emit MintFinished();
+        return true;
+    }
+}
+
+contract TalantCoin is MintableToken, Pausable {
+
+    using SafeMath for uint256;
+
+    string  public name = "TALENTICO";
+    string  public symbol = "TLN";
+    uint256 constant public decimals = 18;
+    uint256 constant dec = 10**decimals;
+    uint256 public initialSupply = 12000000000 * dec;
+    address public crowdsaleAddress;
+
+    modifier onlyICO() {
+        require(msg.sender == crowdsaleAddress);
+        _;
+    }
+
+    function TalantCoin() public {
+        totalSupply_ = totalSupply_.add(initialSupply);
+        balances[owner] = balances[owner].add(initialSupply);
+        emit Transfer(address(0x0), this, initialSupply);
+    }
+
+    function setSaleAddress(address _saleaddress) public onlyOwner{
+        crowdsaleAddress = _saleaddress;
+    }
+
+    function transferFromICO(address _to, uint256 _value) public onlyICO returns(bool) {
+        require(_to != address(0x0));
+        return super.transfer(_to, _value);
+    }
+
+    function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {
+        return super.transfer(_to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
+        return super.transferFrom(_from, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public whenNotPaused returns (bool) {
+        return super.approve(_spender, _value);
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public whenNotPaused returns (bool success) {
+        return super.increaseApproval(_spender, _addedValue);
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public whenNotPaused returns (bool success) {
+        return super.decreaseApproval(_spender, _subtractedValue);
+    }
+} 
