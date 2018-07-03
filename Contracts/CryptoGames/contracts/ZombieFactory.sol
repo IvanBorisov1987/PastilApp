73ff3d82d75efcd8830b435167a53a9d3aa54926
PillarDevelopment <2622610@gmail.com>
2018-07-04 01:31:04 +0300

    готов базовый функционал фабрики

--- Contracts/CryptoGames/contracts/ZombieFactory.sol
+++ Contracts/CryptoGames/contracts/ZombieFactory.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 	
+	event NewZombie(uint zombieId, string name, uint dna);// Здесь объяви событие
+	
 	uint dnaDigits = 16;
 	uint dnaModulus = 10 ** dnaDigits;
 	
@@ -13,7 +15,8 @@ contract ZombieFactory {
 	Zombie[] public zombies;
 	
 	function _createZombie(string _name, uint _dna) private {
-		zombies.push(Zombie(_name, _dna));
+		uint id = zombies.push(Zombie(_name, _dna)) - 1;
+		NewZombie(id, _name, _dna);
 	}
 	
 	function _generateRandomDna(string _str) private view returns (uint) {
@@ -21,9 +24,8 @@ contract ZombieFactory {
 		return rand % dnaModulus;
 	}
 	
-	function createRandomZombie(string _name) public view returns (uint) {
-
+	function createRandomZombie(string _name) public {
+		uint randDna = _generateRandomDna(_name);
+		_createZombie(_name, randDna);
 	}
-// Начало здесь
-
 }
